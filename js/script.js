e04a3c0a33dcee0b7610eefb96d08bb9120b6529
emmanuel kiptunge <emanutunge@gmail.com>
2022-01-21 07:27:23 +0300

    alert function added

--- js/script.js
+++ js/script.js
@@ -15,5 +15,44 @@ $('document').ready(function () {
         $("#dev").hide("2000");
         $("#dev-image").show("2000");
     })
-    
-})
+    $("#product-image").click(function() {
+        $("#product-image").hide("2000");
+        $("#product").show("2000");
+    })
+    $("#product").click(function() {
+        $("#product").hide("2000");
+        $("#produt-image").show("2000");
+    })
+    //portfolio section
+    $("#work4").hover(function() {
+        $("#mouse-hover1").toggle();
+    });
+    $("#work3").hover(function() {
+        $("#mouse-hover2").toggle();
+    });
+    $("#work2").hover(function() {
+        $("mouse-hover3").toggle();
+    });
+    $("#work1").hover(function() {
+        $("#mouse-hover4").toggle();
+    });
+    $("#work5").hover(function() {
+        $("#mouse-hover5").toggle();
+    });
+    $("#work6").hover(function() {
+        $("#mouse-hover6").toggle();
+    });
+    $("#work7").hover(function() {
+        $("#mouse-hover7").toggle();
+    });
+    $("#work8").hover(function() {
+        $("#mouse-hover8").toggle();
+    });
+
+    //
+    $("form#form1").submit(function(e){
+        var name = $("#mce-NAME").val();
+        e.preventDefault();
+        alert("Hello!" + name + "Thank you for reaching out");
+    })
+});
