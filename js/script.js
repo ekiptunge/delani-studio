936adde60649aef848648da83a0838aa70a12d9c
emmanuel kiptunge <emanutunge@gmail.com>
2022-01-21 06:39:14 +0300

    design and image function added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $('document').ready(function () {
     $("#design-image").click(function() {
-        $('#design-image').hide('2000');
-        $('#design').show('2000');
+        $("#design-image").hide("2000");
+        $("#design").show("2000");
     })
-}
+    $("#design").click(function() {
+        $("#design").hide("2000");
+        $("#design-image").show("2000")
+    })
+    $("#dev-image").click(function() {
+        $("#dev-image").hide("2000");
+        $("#dev").show("2000");
+    })
+    $("#dev").click(function() {
+        $("#dev").hide("2000");
+        $("#dev-image").show("2000");
+    })
+    
+})
